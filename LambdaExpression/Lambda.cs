c9acaecf7109c59affa0f8e20a6b6e08de1138a1
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-06-22 20:24:25 +0530

    [akhila]added:lambda expression

---
+++ LambdaExpression/Lambda.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LambdaExpression
+{
+     public class  Person
+     {       
+            public int Age { get; set; }
+            public string Name { get; set; }
+            public string Address { get; set; }
+            public int Pincode { get; set; }
+
+        
+    }
+}
---
+++ LambdaExpression/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LambdaExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Lambda Expression");
+            List<Person> listofpersons = new List<Person>();
+            listofpersons.Add(new Person() { Address = "Hyderabad", Age = 16, Name = "amit", Pincode = 678905 });
+            listofpersons.Add(new Person() { Address = "Mumbai", Age = 25, Name = "raju", Pincode = 123456 });
+            listofpersons.Add(new Person() { Address = "Chennai", Age = 9, Name = "sonu", Pincode = 509132 });
+            listofpersons.Add(new Person() { Address = "Karnataka", Age = 30, Name = "vani", Pincode = 509345 });
+            var temp_person = listofpersons.Find(x => x.Age == 30);
+            var temp_personTwo = listofpersons.FindAll(x => x.Age > 15);
+            foreach (var ele in temp_personTwo)
+            {
+                Console.WriteLine(ele.Address);
+            }
+            Console.WriteLine(temp_person.Name);
+        }
+    }
+}
