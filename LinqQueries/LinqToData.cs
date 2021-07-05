cf04acc977d8d3d3d2bdee506722f4702d4859e2
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-07-05 08:39:42 +0530

    [akhila]Refactor:Linq queries in sql

---
+++ LinqQueries/LinqToData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace LinqQueries
+{
+    public class LinqToData
+    {
+        public void AddData()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("ID");
+            table.Columns.Add("ProductName");
+
+            table.Rows.Add("1", "Sanitizer");
+            table.Rows.Add("2", "Dettol");
+            table.Rows.Add("3", "HandWash");
+            DisplayProducts(table);
+        }
+
+        private void DisplayProducts(DataTable table)
+        {
+            var productNames = from products in table.AsEnumerable() select products.Field<string>("ProductName");
+            Console.WriteLine("product names: ");
+            foreach (string productName in productNames)
+            {
+                Console.WriteLine(productName);
+            }
+        }
+    }
+}
---
+++ LinqQueries/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LinqQueries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linq Queries");
+            LinqToData linqToData = new LinqToData();
+            linqToData.AddData();
+        }
+    }
+}
